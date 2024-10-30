95d7ebeb354afdf3fcd6403d81f1d3879e42336a
haejeg <haejeg@gmail.com>
2024-10-29 22:40:58 -0600

    Tried to add stuff, but there's an error for users_db not existing if anyone can fix that, I think it's because of either docker-compose.yaml or create.sql

---
+++ ProjectSourceCode/init_data/create.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE users_db;
+
+CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    password CHAR(60) NOT NULL
+);
--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -1,10 +1,73 @@
+// *****************************************************
+// <!-- Section 1 : Import Dependencies -->
+// *****************************************************
+
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const handlebars = require('express-handlebars');
+const path = require('path');
+const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const axios = require('axios');
+
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials'),
+});
+
+const dbConfig = {
+  host: 'db',
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+
+const db = pgp(dbConfig);
+
+db.connect()
+  .then((obj) => {
+    console.log('Database connection successful');
+    obj.done();
+  })
+  .catch((error) => {
+    console.log('ERROR:', error.message || error);
+  });
+
+// *****************************************************
+// <!-- Section 3 : App Settings -->
+// *****************************************************
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
 
 
+// API CALLS FROM HERE!!!
+// Redirect to login page for now could change later - Danny
+app.get('/', (req, res) => {
+    res.redirect('/login');
+});
 
-app.listen(PORT, function(err){
-    if (err) console.log("Error in server setup")
-    console.log("Server listening on Port", PORT);
-})
+// This function will specify the specific render from the directory - Danny
+app.get('/login', (req, res) => {
+    res.render('pages/login'); 
+});
+
+// No matter what, don't delete this it's necessary - Danny
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
