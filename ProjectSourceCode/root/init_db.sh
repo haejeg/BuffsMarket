f837ffa3e71b5d3e5db1df788949890fec3dfeea
damo8852 <damo8852@colorado.edu>
2024-11-21 10:25:54 -0700

    init_db.sh directory path

--- ProjectSourceCode/root/init_db.sh
+++ ProjectSourceCode/root/init_db.sh
@@ -7,7 +7,7 @@
 PG_URI="<postgresql://users_db_idl3_user:o0TBQ2FOpKMNfXF58E0lUbt96pDJr0ct@dpg-csvmck56l47c73dojvog-a.oregon-postgres.render.com/users_db_idl3>"
 
 # Execute each .sql file in the directory
-for file in init_data/*.sql; do
+for file in ../init_data/*.sql; do
     echo "Executing $file..."
     psql $PG_URI -f "$file"
 done
