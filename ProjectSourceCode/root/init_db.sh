0158ba35f47c880ca4394d992a947cb2f8b4c429
damo8852 <damo8852@colorado.edu>
2024-11-30 17:04:35 -0600

    docker compose

--- ProjectSourceCode/root/init_db.sh
+++ ProjectSourceCode/root/init_db.sh
@@ -4,9 +4,8 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="<postgresql://users_db_idl3_user:o0TBQ2FOpKMNfXF58E0lUbt96pDJr0ct@dpg-csvmck56l47c73dojvog-a.oregon-postgres.render.com/users_db_idl3>"
+PG_URI="postgresql://users_db_idl3_user:o0TBQ2FOpKMNfXF58E0lUbt96pDJr0ct@dpg-csvmck56l47c73dojvog-a.oregon-postgres.render.com/users_db_idl3"
 
-# Execute each .sql file in the directory
 for file in ../init_data/*.sql; do
     echo "Executing $file..."
     psql $PG_URI -f "$file"
