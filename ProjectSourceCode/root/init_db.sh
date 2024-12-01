5dffa1a0eef43660feca6ba052c0a55a6a12002b
damo8852 <damo8852@colorado.edu>
2024-11-30 19:14:06 -0600

    cloud test

--- ProjectSourceCode/root/init_db.sh
+++ ProjectSourceCode/root/init_db.sh
@@ -4,8 +4,9 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://users_db_idl3_user:o0TBQ2FOpKMNfXF58E0lUbt96pDJr0ct@dpg-csvmck56l47c73dojvog-a.oregon-postgres.render.com/users_db_idl3"
+PG_URI="postgresql://users_db_3ccg_user:CGYpvOvfEM5y0z4W6QzJnZeUajNVWfPP@dpg-ct5rattumphs738ukeh0-a.oregon-postgres.render.com/users_db_3ccg"
 
+# Execute each .sql file in the directory
 for file in init_data/*.sql; do
     echo "Executing $file..."
     psql $PG_URI -f "$file"
